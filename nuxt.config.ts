cecc2fa14b6035a1c3e4ce04b72d52ed039d1fe7
Amirali Mirzaei <mirzaeiamirali111@gmail.com>
2026-07-01 04:18:36 -0700

    Add fonts and icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,21 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
    devtools: { enabled: true },
      modules: ['@nuxt/icon'],
-       css: ['@/assets/styles/main.scss']
+       css: ['@/assets/styles/main.scss'],
+       app: {
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com',
+          crossorigin: '',
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Mozilla+Text:wght@200..700&display=swap',
+        },
+      ],
+    },
+  },
 })

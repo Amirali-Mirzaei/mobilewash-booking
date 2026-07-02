871316f4feba9d967d6ceb03205db30961d98317
Amirali Mirzaei <mirzaeiamirali111@gmail.com>
2026-07-02 04:54:30 -0700

    Add Banner

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,22 @@
-<template>
+<template> 
+
+    <img 
+      :src="banner"
+        class="banner">
+
 </template>
 
 <script setup>
+import banner from '@/assets/images/Banner.PNG'
 </script>
 
 <style lang="scss" scoped>
+
+.banner {
+background-size: cover;
+width: 100%;
+height: auto;
+margin-top: 6%;
+}
+
 </style>
